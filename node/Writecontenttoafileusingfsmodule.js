b2a86d9fcc868b468c2a453d6dae2582bd86b402
Ajai-3 <ajaipjayan@gmail.com>
2025-06-20 17:48:17 +0530

    Add initial implementation for file writing using fs module

---
+++ node/Writecontenttoafileusingfsmodule.js
@@ -0,0 +1,10 @@
+import fs from "fs"
+
+fs.writeFile("./text.txt", "helllooo", (err) => {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log("File created")
+    return 
+})
