44a34062d2cc33bf229ba1b631f205d9dd156977
Ajai-3 <ajaipjayan@gmail.com>
2025-06-22 22:56:12 +0530

    Remove unlink operation from file writing callback

--- node/Writecontenttoafileusingfsmodule.js
+++ node/Writecontenttoafileusingfsmodule.js
@@ -5,7 +5,11 @@ fs.writeFile("./text.txt", "helllooo", (err) => {
         console.log(err)
         return
     }
-    
+    fs.unlink("./text.txt", (err) => {
+        if (err) {
+            console.log(err)
+        }
+    })
     console.log("File created")
     return 
 })
