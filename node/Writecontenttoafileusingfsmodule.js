84b079c64750b07219bb76b81680d1c277bc7e82
Ajai-3 <ajaipjayan@gmail.com>
2025-06-22 22:54:08 +0530

    Fix return statement in error handling for file writing

--- node/Writecontenttoafileusingfsmodule.js
+++ node/Writecontenttoafileusingfsmodule.js
@@ -3,7 +3,9 @@ import fs from "fs"
 fs.writeFile("./text.txt", "helllooo", (err) => {
     if (err) {
         console.log(err)
-        return {
+        return
+    }
+    fs.unlink("./text.txt", (err) => {
         if (err) {
             console.log(err)
         }
